90ebf25b65f7dcb4b867fc2d204d8c5c44e01cbf
NoelHerr96 <81242000+NoelHerr96@users.noreply.github.com>
2024-02-12 13:11:25 +0100

    ny sølv oppg 2024

---
+++ public/silver.py
@@ -0,0 +1,33 @@
+# Helt sikker Sølv
+
+import random
+
+a = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ" + \
+    "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ "
+
+dasjdioj = input(a[50] + a[74] + a[81] + a[72] + a[85] + a[94] + a[72] + a[77] +
+                 a[77] + a[94] + a[81] + a[72] + a[74] + a[83] + a[72] + a[70] +
+                 a[94] + a[74] + a[78] + a[67] +
+                 a[68] + a[25] + a[94])
+dasjdioj = int(dasjdioj)
+
+
+def dwioajs():
+    weouhosi = random.randint(a[16] + a[15] + a[15] + a[15]), (a[24]+ a[24]+ a[24]+ a[24])
+    if dasjdioj == weouhosi:
+        print(a[42] + a[78] + a[67] + a[68] + a[77] + a[94] + a[68] + a[81] + a[25] + a[94] + weouhosi)
+    return weouhosi
+
+def dfhsdsd():
+    soihasjn = (a[19] + a[17] + a[15] + a[21])
+    soihasjn = int(soihasjn)
+    return soihasjn
+
+
+dsicjsis = dfhsdsd()
+
+if dasjdioj != dsicjsis:
+    print(a[35] + a[68] + a[83] + a[83] + a[68] + a[94] + a[68] + a[81] +
+          a[94] + a[69] + a[68] + a[72] + a[75] + a[13])
+else:
+    print(a[35] + a[68] + a[83] + a[83] + a[68] + a[94] + a[68] + a[81] + a[94] + a[81] + a[72] + a[74]+ a[83] + a[72] + a[70] + a[13])
